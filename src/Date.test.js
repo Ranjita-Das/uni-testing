9e2f5291f192b23c458304df408be4dae3b3c4c9
Ranjita Das <ranjitadas@Ranjitas-MacBook-Pro.local>
2018-02-17 01:08:13 -0800

    removed date

--- src/Date.test.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import DateComponent from './Date.js';
-import renderer from 'react-test-renderer';
-
-it('renders date correctly',()=>{
-  const tree=renderer.create(<DateComponent />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
